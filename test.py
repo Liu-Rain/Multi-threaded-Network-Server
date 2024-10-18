3c2f71f651e5ef23aa386a4e4f8696aee704527f
劉人豪🐮和李美霖☁️的無敵可愛大mac🍔 <limeilin@limeilindeMacBook-Pro.local>
2024-10-18 12:24:55 +1030

    10.18 morning

--- test.py
+++ test.py
@@ -1,6 +1,65 @@
 import socket
 import threading
 
+class Node:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.next = None
+        self.book_next = None
+
+class Share_list:
+    def __init__(self):
+        self.head = Node("head","head")
+    
+    def print_book(self):
+        finish = []
+        current = self.head.next
+        finish.append(current)
+
+        while current != None:
+
+            if current.name not in finish:
+                current_read = current
+
+                while current_read != None:
+                    print(current_read.data)
+                    current_read = current_read.book_next
+
+                finish.append(current.name)
+            current = current.next
+    def get_head(self):
+        return self.head
+    def insert(self, name, data):
+        new_node = Node(name, data)
+        book = {}
+
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
+            
+            book.update({current_node.name:current_node}) 
+        
+        current_node.next = new_node
+        book[name] = new_node
+        print(book)
+    
+            
+        
+        
+x = Share_list()
+x.insert("book1", "dwadas")
+x.insert("book2", "dwadas")
+x.insert("book3", "dwadas")
+x.insert("book1", "dwadas")
+x.insert("book2", "dwadas")
+x.insert("book3", "dwadas")
+
+
+    
+
+
+
 # Function to handle a client connection
 def handle_client(clientsocket, address):
     print(f"Connection from {address} has been established.")
@@ -15,7 +74,7 @@ def handle_client(clientsocket, address):
                 break  # Exit if the client disconnects
             
             # Print the received data
-            print(f"Received from {address}: {data.decode('utf-8')}")
+            #print(f"Received from {address}: {data.decode('utf-8')}")
             
             # Send a response to the client
             clientsocket.send(b"Data received!\n")
@@ -42,6 +101,8 @@ print(f"Server started on {host}:{port}")
 # Start listening for incoming connections
 serversocket.listen(5)
 
+node_array = []
+
 while True:
     # Accept an incoming connection
     clientsocket, address = serversocket.accept()
@@ -49,5 +110,8 @@ while True:
     # Create a new thread to handle the client connection
     client_thread = threading.Thread(target=handle_client, args=(clientsocket, address))
     client_thread.start()
+    if client_thread.name not in node_array:
+        node_array.append(client_thread.name)
 
     print(f"Started thread {client_thread.name} for {address}")
+    print(node_array)
